9b6f4a04e469bc897a5d202a41cf4fd3bcc12878
Jack Douglas <jack.douglas10@outlook.com>
2018-06-21 12:50:16 +0100

    Moved commands back to server side - illusive
    Added invailid arguments

--- socials/server.lua
+++ socials/server.lua
@@ -1,55 +1,94 @@
 local lifeinvader_webhook_enabled = 0
-local lifeinvader_webhook = "REPLACE ME"
+local lifeinvader_webhook = "REPLACE ME IF ABOVE IS 1" 
 local tweet_webhook_enabled = 0
-local tweet_webhook = "REPLACE ME"
+local tweet_webhook = "REPLACE ME IF ABOVE IS 1" 
 local snapmatic_webhook_enabled = 0
-local snapmatic_webhook = "REPLACE ME"
-
--- Send chatMessage when command is done
-RegisterServerEvent('command:lifeinvader')
-AddEventHandler('command:lifeinvader', function(name, username, message)
-    CancelEvent()
-    TriggerClientEvent('chatMessage', -1, "^0[^1Life^0invader] ^0(" .. name .. ") ^3" .. username, { 128, 128, 128 }, message)
-end)
-
-RegisterServerEvent('command:twitter')
-AddEventHandler('command:twitter', function(name, username, message)
-    CancelEvent()
-    TriggerClientEvent('chatMessage', -1, "^0[^4Twitter^0] ^0(" .. name .. ") ^3" .. username, { 128, 128, 128 }, message)
-end)
-
-RegisterServerEvent('command:snapmatic')
-AddEventHandler('command:snapmatic', function(name, username, message)
-    CancelEvent()
-    TriggerClientEvent('chatMessage', -1, "^0[^6Snapmatic^0] ^0(" .. name .. ") ^3" .. username, { 128, 128, 128 }, message)
-end)
-
--- Webhooks to send logs to discord
-RegisterServerEvent('webhook:lifeinvader')
-AddEventHandler('webhook:lifeinvader', function(name, message)
-    if lifeinvader_webhook_enabled == 1 then
-        -- Send message to discord to say that the Lifeinvader command was used.
-        lv_wh_content = "**Lifeinvader** " ..name.. " used Lifeinvader command: **" .. message .. "**"
-        PerformHttpRequest(lifeinvader_webhook, process, "POST", "content=".. lv_wh_content)
+local snapmatic_webhook = "REPLACE ME IF ABOVE IS 1" 
+ 
+AddEventHandler('chatMessage', function(source, name, args) 
+  sm = stringsplit(args, " "); 
+  if tablelength(sm) < 3 then
+    CancelEvent() 
+    if sm[1] == "/lv" then
+        TriggerClientEvent('invalidArgs', source, 'lv')
+    elseif sm[1] == "/tweet" then
+        TriggerClientEvent('invalidArgs', source, 'tweet')
+    elseif sm[1] == "/snapmatic" then
+        TriggerClientEvent('invalidArgs', source, 'snapmatic')
     end
-end)
+  else
+    if sm[1] == "/lv" then 
+            CancelEvent()
+            local username = sm[2] .. ' ' .. sm[3] 
+    
+            table.remove(sm, 1) 
+            table.remove(sm, 1) 
+            table.remove(sm, 1) 
+    
+            local message = table.concat(sm, " ") 
+            
+            if lifeinvader_webhook_enabled == 1 then 
+                -- Send message to discord to say that the Lifeinvader command was used. 
+                lv_wh_content = "**Lifeinvader** " ..name.. " used Lifeinvader command: **" .. message .. "**" 
+                PerformHttpRequest(lifeinvader_webhook, process, "POST", "content=".. lv_wh_content) 
+            end 
 
-RegisterServerEvent('webhook:twitter')
-AddEventHandler('webhook:twitter', function(name, message)
-    if tweet_webhook_enabled == 1 then
-        -- Send message to discord to say that the Lifeinvader command was used.
-        tw_wh_content = "**Twitter** " ..name.. " used Twitter command: **" .. message .. "**"
-        PerformHttpRequest(tweet_webhook, process, "POST", "content=".. tw_wh_content)
+            TriggerClientEvent('chatMessage', -1, "^0[^1Life^0invader] ^0(" .. name .. ") ^3" .. username, { 128, 128, 128 }, message) 
+        elseif sm[1] == "/tweet" then 
+            CancelEvent() 
+            local username = sm[2] .. ' ' .. sm[3] 
+    
+            table.remove(sm, 1) 
+            table.remove(sm, 1) 
+            table.remove(sm, 1) 
+    
+            local message = table.concat(sm, " ") 
+            
+            if tweet_webhook_enabled == 1 then 
+                -- Send message to discord to say that the Lifeinvader command was used. 
+                tw_wh_content = "**Twitter** " ..name.. " used Twitter command: **" .. message .. "**" 
+                PerformHttpRequest(tweet_webhook, process, "POST", "content=".. tw_wh_content) 
+            end 
+    
+            TriggerClientEvent('chatMessage', -1, "^0[^4Twitter^0] ^0(" .. name .. ") ^3" .. username, { 128, 128, 128 }, message) 
+        elseif sm[1] == "/snapmatic" then 
+            CancelEvent() 
+            local username = sm[2] .. ' ' .. sm[3] 
+    
+            table.remove(sm, 1) 
+            table.remove(sm, 1) 
+            table.remove(sm, 1) 
+    
+            local message = table.concat(sm, " ") 
+            
+            if snapmatic_webhook_enabled == 1 then 
+                -- Send message to discord to say that the Lifeinvader command was used. 
+                sm_wh_content = "**Scapmatic** " ..name.. " used Snapmatic command: **" .. message .. "**" 
+                PerformHttpRequest(snapmatic_webhook, process, "POST", "content=".. sm_wh_content) 
+            end 
+    
+            TriggerClientEvent('chatMessage', -1, "^0[^6Snapmatic^0] ^0(" .. name .. ") ^3" .. username, { 128, 128, 128 }, message) 
+        elseif sm[1] == "/social-help" then 
+            CancelEvent() 
+            TriggerClientEvent('socialHelp', source) 
+        end 
     end
-end)
+end) 
+ 
+function stringsplit(inputstr, sep) 
+    if sep == nil then 
+        sep = "%s" 
+    end 
+    local t={} ; i=1 
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do 
+        t[i] = str 
+        i = i + 1 
+    end 
+    return t 
+end
 
-RegisterServerEvent('webhook:snapmatic')
-AddEventHandler('webhook:snapmatic', function(name, message)
-    if snapmatic_webhook_enabled == 1 then
-        -- Send message to discord to say that the Lifeinvader command was used.
-        sm_wh_content = "**Scapmatic** " ..name.. " used Snapmatic command: **" .. message .. "**"
-        PerformHttpRequest(snapmatic_webhook, process, "POST", "content=".. sm_wh_content)
-    end
-end)
-
-print('Socials > Successfully Loaded')
+function tablelength(T)
+    local count = 0
+    for _ in pairs(T) do count = count + 1 end
+    return count
+  end
