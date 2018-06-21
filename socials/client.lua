48fae5e8d7ebd7195f75377c217ef13aafb14dce
Jack Douglas <jack.douglas10@outlook.com>
2018-06-21 03:41:40 +0100

    Transfered over to RegisterCommand
    Kept webhooks server side

--- socials/client.lua
+++ socials/client.lua
@@ -1,3 +1,51 @@
+RegisterCommand('lv', function(source, args)
+    
+    local name = GetPlayerName(_source)
+    local username = args[1] .. ' ' .. args[2]
+
+    table.remove(args, 1)
+    table.remove(args, 1)
+
+    local message = table.concat(args, " ")
+    
+    TriggerServerEvent('webhook:lifeinvader', name, message)
+    
+    TriggerEvent('chatMessage', "^0[^1Life^0invader] ^0(" .. name .. ") ^3" .. username .. '^0: ' .. message)
+end, false)
+
+RegisterCommand('tweet', function(source, args)
+    local name = GetPlayerName(_source)
+    local username = args[1] .. ' ' .. args[2]
+
+    table.remove(args, 1)
+    table.remove(args, 1)
+
+    local message = table.concat(args, " ")
+
+    TriggerServerEvent('webhook:twitter', name, message)
+    
+    TriggerEvent('chatMessage', "^0[^4Twitter^0] ^0(" .. name .. ") ^3" .. username .. '^0: ' .. message)
+end, false)
+
+RegisterCommand('snapmatic', function(source, args)
+    local name = GetPlayerName(_source)
+    local username = args[1] .. ' ' .. args[2]
+
+    table.remove(args, 1)
+    table.remove(args, 1)
+
+    local message = table.concat(args, " ")
+
+    TriggerServerEvent('webhook:snapmatic', name, message)
+    
+    TriggerEvent('chatMessage', "^0[^6Snapmatic^0] ^0(" .. name .. ") ^3" .. username .. '^0: ' .. message)
+end, false)
+
+-- Shows the help information
+RegisterCommand('social-help', function(source, args)
+    TriggerEvent('socialHelp')
+end, false)
+
 RegisterNetEvent('socialHelp')
 
 AddEventHandler('socialHelp', function() -- adds an event handler so it can be registered
--- socials/server.lua
+++ socials/server.lua
@@ -1,77 +1,33 @@
 local lifeinvader_webhook_enabled = 0
-local lifeinvader_webhook = "REPLACE ME IF ABOVE IS 1"
+local lifeinvader_webhook = "REPLACE ME"
 local tweet_webhook_enabled = 0
-local tweet_webhook = "REPLACE ME IF ABOVE IS 1"
+local tweet_webhook = "REPLACE ME"
 local snapmatic_webhook_enabled = 0
-local snapmatic_webhook = "REPLACE ME IF ABOVE IS 1"
+local snapmatic_webhook = "REPLACE ME"
 
-AddEventHandler('chatMessage', function(source, name, args)
-	sm = stringsplit(args, " ");
-	if sm[1] == "/lv" then
-        CancelEvent()
-        local username = sm[2] .. ' ' .. sm[3]
-
-        table.remove(sm, 1)
-        table.remove(sm, 1)
-        table.remove(sm, 1)
-
-        local message = table.concat(sm, " ")
-        
-        if lifeinvader_webhook_enabled == 1 then
-            -- Send message to discord to say that the Lifeinvader command was used.
-            lv_wh_content = "**Lifeinvader** " ..name.. " used Lifeinvader command: **" .. message .. "**"
-            PerformHttpRequest(lifeinvader_webhook, process, "POST", "content=".. lv_wh_content)
-        end
-
-        TriggerClientEvent('chatMessage', -1, "^0[^1Life^0invader] ^0(" .. name .. ") ^3" .. username, { 128, 128, 128 }, message)
-    elseif sm[1] == "/tweet" then
-        CancelEvent()
-        local username = sm[2] .. ' ' .. sm[3]
-
-        table.remove(sm, 1)
-        table.remove(sm, 1)
-        table.remove(sm, 1)
-
-        local message = table.concat(sm, " ")
-        
-        if tweet_webhook_enabled == 1 then
-            -- Send message to discord to say that the Lifeinvader command was used.
-            tw_wh_content = "**Twitter** " ..name.. " used Twitter command: **" .. message .. "**"
-            PerformHttpRequest(tweet_webhook, process, "POST", "content=".. tw_wh_content)
-        end
-
-        TriggerClientEvent('chatMessage', -1, "^0[^4Twitter^0] ^0(" .. name .. ") ^3" .. username, { 128, 128, 128 }, message)
-    elseif sm[1] == "/snapmatic" then
-        CancelEvent()
-        local username = sm[2] .. ' ' .. sm[3]
-
-        table.remove(sm, 1)
-        table.remove(sm, 1)
-        table.remove(sm, 1)
-
-        local message = table.concat(sm, " ")
-        
-        if snapmatic_webhook_enabled == 1 then
-            -- Send message to discord to say that the Lifeinvader command was used.
-            sm_wh_content = "**Scapmatic** " ..name.. " used Snapmatic command: **" .. message .. "**"
-            PerformHttpRequest(snapmatic_webhook, process, "POST", "content=".. sm_wh_content)
-        end
-
-        TriggerClientEvent('chatMessage', -1, "^0[^6Snapmatic^0] ^0(" .. name .. ") ^3" .. username, { 128, 128, 128 }, message)
-    elseif sm[1] == "/social-help" then
-        CancelEvent()
-        TriggerClientEvent('socialHelp', source)
-	end
+RegisterServerEvent('webhook:lifeinvader')
+AddEventHandler('webhook:lifeinvader', function(name, message)
+    if lifeinvader_webhook_enabled == 1 then
+        -- Send message to discord to say that the Lifeinvader command was used.
+        lv_wh_content = "**Lifeinvader** " ..name.. " used Lifeinvader command: **" .. message .. "**"
+        PerformHttpRequest(lifeinvader_webhook, process, "POST", "content=".. lv_wh_content)
+    end
 end)
 
-function stringsplit(inputstr, sep)
-    if sep == nil then
-        sep = "%s"
+RegisterServerEvent('webhook:twitter')
+AddEventHandler('webhook:twitter', function(name, message)
+    if tweet_webhook_enabled == 1 then
+        -- Send message to discord to say that the Lifeinvader command was used.
+        tw_wh_content = "**Twitter** " ..name.. " used Twitter command: **" .. message .. "**"
+        PerformHttpRequest(tweet_webhook, process, "POST", "content=".. tw_wh_content)
     end
-    local t={} ; i=1
-    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
-        t[i] = str
-        i = i + 1
+end)
+
+RegisterServerEvent('webhook:snapmatic')
+AddEventHandler('webhook:snapmatic', function(name, message)
+    if snapmatic_webhook_enabled == 1 then
+        -- Send message to discord to say that the Lifeinvader command was used.
+        sm_wh_content = "**Scapmatic** " ..name.. " used Snapmatic command: **" .. message .. "**"
+        PerformHttpRequest(snapmatic_webhook, process, "POST", "content=".. sm_wh_content)
     end
-    return t
-end
+end)
