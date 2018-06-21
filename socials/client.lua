f3161a1331977f86d63347fb7c28fdd70e27c911
Jack Douglas <jack.douglas10@outlook.com>
2018-06-21 04:37:41 +0100

    Added about to help

--- socials/client.lua
+++ socials/client.lua
@@ -49,6 +49,7 @@ end, false)
 RegisterNetEvent('socialHelp')
 
 AddEventHandler('socialHelp', function() -- adds an event handler so it can be registered
+    drawNotification("~y~Socials~w~: Created by ~y~JackD")
     drawNotification("~r~Lifeinvader~w~: /lv (first name) (last name) (message)")
     drawNotification("~b~Twitter~w~: /tweet (first name) (last name) (message)")
     drawNotification("~p~Snapmatic~w~: /snapmatic (first name) (last name) (message)")
